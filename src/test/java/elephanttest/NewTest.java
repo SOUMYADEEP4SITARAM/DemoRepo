14cb75060351ac144ea463b11c2830bcdbf28c90
soumyadeep4sitaram <soumyadeep4sitaram@gmail.com>
2018-07-04 01:14:50 +0530

    my first git code mahadev

---
+++ src/test/java/elephanttest/NewTest.java
@@ -0,0 +1,20 @@
+package elephanttest;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {    
+	  System.out.println("kkk");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
